ccef6a7ea9709dedf46939edc655f645e3d6e752
whitcr <whitcr0k@gmail.com>
2024-04-20 20:33:28 +0200

    added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,56 @@
+import React, { useState, useEffect } from 'react';
+import ContactList from './ContactList/ContactList';
+import Filter from './Filter/Filter';
+import ContactForm from './ContactForm/ContactForm';
+
 export const App = () => {
+  const [contacts, setcontacts] = useState(
+    JSON.parse(localStorage.getItem('contacts'))
+  );
+
+  const [filter, setfilter] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const handleFilterChange = e => {
+    setfilter(e.target.value);
+  };
+
+  const addContact = newContact => {
+    const isDuplicate = contacts.some(
+      contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
+    );
+
+    if (isDuplicate) {
+      alert(`${newContact.name} is already in contacts. `);
+      return;
+    }
+    setcontacts(prev => [...prev, newContact]);
+  };
+
+  const deleteContact = contactId => {
+    setcontacts(prev => prev.filter(contact => contact.id !== contactId));
+  };
+
+  const getFilteredContacts = () => {
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
+
+  const filteredContacts = getFilteredContacts();
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm onSubmit={addContact} />
+      <h2>Contacts</h2>
+      <Filter value={filter} onChange={handleFilterChange} />
+      <ContactList
+        contacts={filteredContacts}
+        onDeleteContact={deleteContact}
+      />
     </div>
   );
 };
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import { nanoid } from 'nanoid';
+
+const ContactForm = ({ onSubmit }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const handleNameChange = e => {
+    setName(e.target.value);
+  };
+
+  const handleNumberChange = e => {
+    setNumber(e.target.value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    const newContact = { id: nanoid(), name, number };
+    onSubmit(newContact);
+    setName('');
+    setNumber('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name:
+        <input
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          value={name}
+          onChange={handleNameChange}
+          required
+        />
+      </label>
+      <label>
+        Number:
+        <input
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          value={number}
+          onChange={handleNumberChange}
+          required
+        />
+      </label>
+      <button type="submit">Add Contact</button>
+    </form>
+  );
+};
+
+export default ContactForm;
